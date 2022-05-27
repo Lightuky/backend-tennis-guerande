1445d44165f2a58c6263a3c8cb3cf51aebeb1d8b
VicAffile <victoraffile@gmail.com>
2022-05-27 23:47:35 +0200

    upd code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const router = require("./router/index");
+const router = require("./src/router/index");
 const config = require("./config");
 
 //const db = require("./db/connection");
