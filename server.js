8d0b73777214a561069020f805d6236a328f8994
Lightuky <47103897+Lightuky@users.noreply.github.com>
2023-03-08 15:16:03 +0100

    add cors between frontend and backend

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-import express from "express";
+import express, {json} from "express";
+import cors from "cors";
 
 import { router } from "./src/router/index.js";
 import { config } from "./config.js";
@@ -6,8 +7,10 @@ import { config } from "./config.js";
 import { connection as db } from "./src/db/connection.js";
 
 const app = express();
-
+app.use(cors({origin: "*"}));
 app.use("/", router);
+
+app.use(json())
 
 app.listen(config.PORT, () => {
   console.log(`L'application tourne sur le port ${config.PORT}.`);
